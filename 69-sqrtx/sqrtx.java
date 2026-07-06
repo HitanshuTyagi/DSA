8160e3842e1042892ce8abe2db9422e073f35fcf
HitanshuTyagi <156786627+HitanshuTyagi@users.noreply.github.com>
2026-07-06 10:31:25 +0530

    Time: 57 ms (5.03%) | Memory: 42.7 MB (22.97%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if(x<2)return x;
+        int ans=0;
+        for(long i=1;i<=x/2;i++){
+            long mul=i*i;
+            if(mul<=x){
+                ans=(int)i;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
     }
 }
